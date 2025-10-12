d439dbe9d8aa468334e8c34f5b777a7b59603b7e
Kote Papiashvili <kote.papiashvili01@gmail.com>
2025-10-12 14:24:50 +0400

    init commit

--- src/main/java/com/example/educonnect/controller/UserController.java
+++ src/main/java/com/example/educonnect/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.educonnect.controller;
 
+import com.example.educonnect.dto.LoginRequestDto;
+import com.example.educonnect.dto.LoginResponseDto;
 import com.example.educonnect.dto.UserRequestDto;
 import com.example.educonnect.dto.UserResponseDto;
+import com.example.educonnect.service.AuthService;
 import com.example.educonnect.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -13,8 +19,11 @@ import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final AuthService authService;
+    @PostMapping("/register")
     public ResponseEntity<UserResponseDto> registerUser(@RequestBody UserRequestDto userRequestDto){
         UserResponseDto userResponseDto = userService.registerUser(userRequestDto);
         URI location = ServletUriComponentsBuilder
@@ -23,5 +32,10 @@ public class UserController {
                 .buildAndExpand(userResponseDto.getId())
                 .toUri();
         return ResponseEntity.created(location).body(userResponseDto);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto){
+        LoginResponseDto login = authService.login(loginRequestDto);
+        return new ResponseEntity<>(login, HttpStatus.OK);
     }
 }
