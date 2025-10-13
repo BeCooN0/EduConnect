e907482ecdfcf266cca2d028b87cde11fc307850
Kote Papiashvili <kote.papiashvili01@gmail.com>
2025-10-13 14:41:27 +0400

    added PreAuthorize annotation

--- src/main/java/com/example/educonnect/controller/TeacherController.java
+++ src/main/java/com/example/educonnect/controller/TeacherController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,6 +30,7 @@ public class TeacherController {
         return ResponseEntity.created(location).body(teacherResponseDto);
     }
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Page<TeacherResponseDto>> getAllTeacher(@PageableDefault(size = 10, page = 0) Pageable pageable){
         Page<TeacherResponseDto> allTeacher = teacherService.getAllTeacher(pageable);
         return ResponseEntity.ok(allTeacher);
