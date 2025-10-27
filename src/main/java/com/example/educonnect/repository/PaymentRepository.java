4bfcec28c4a6b922aed1b85258d38f60af47101c
Kote Papiashvili <kote.papiashvili01@gmail.com>
2025-10-27 23:44:08 +0400

    fix some issues

--- src/main/java/com/example/educonnect/repository/PaymentRepository.java
+++ src/main/java/com/example/educonnect/repository/PaymentRepository.java
@@ -4,7 +4,7 @@ import com.example.educonnect.entity.Payment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
